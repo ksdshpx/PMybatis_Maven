7b221a19be178d78756f7237f70553b92fab9846
ksdshpx <ksdshpx2018@aliyun.com>
2019-09-03 14:33:01 +0800

    全局配置文件_properties_引入外部配置文件！

--- src/test/java/cn/ksdshpx/mybatis/test/MyBatisTest.java
+++ src/test/java/cn/ksdshpx/mybatis/test/MyBatisTest.java
@@ -22,11 +22,13 @@ public class MyBatisTest {
     /**
      * 1.根据xml配置文件（全局配置文件）创建SqlSessionFactory对象,获取SqlSession实例，能直接执行已经映射的sql语句
      * 有数据源等运行环境信息
-     * 2.sql映射文件，配置了每一个sql以及sql的封装规则等
+     * 2.sql映射文件，配置了每一个sql的映射信息以及sql的封装规则等
      * 3.将sql映射文件注册到全局配置文件中
      * 4.写代码
      * 1）根据全局配置文件得到SqlSessionFactory
-     * 2）使用SqlSessionFactory获取SqlSeesion对象，使用它来执行sql语句，一个SqlSession代表和数据库的一次会话，用完需要关闭
+     * 2）使用SqlSessionFactory获取SqlSeesion对象，使用它来执行sql语句，
+     *    一个SqlSession代表和数据库的一次会话，用完需要关闭,SqlSession对象和Connection一样都是非线程安全的,
+     *    每次使用都应该获取新的对象
      * 3）使用sql的唯一标识告诉MyBatis执行哪个sql,sql都是保存在sql映射文件中
      *
      * @throws IOException
