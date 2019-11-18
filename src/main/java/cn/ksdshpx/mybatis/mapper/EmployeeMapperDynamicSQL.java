c69e716c84655c9295b79ae92028d3653b6b1ad9
ksdshpx <ksdshpx2018@aliyun.com>
2019-11-18 09:52:08 +0800

    MyBatis_动态sql_简介&环境搭建!
    MyBatis_动态sql_简介&环境搭建!

---
+++ src/main/java/cn/ksdshpx/mybatis/mapper/EmployeeMapperDynamicSQL.java
@@ -0,0 +1,12 @@
+package cn.ksdshpx.mybatis.mapper;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.xing
+ * Date: 2019/11/18
+ * Time: 9:50
+ * Description:动态SQL的Mapper接口
+ */
+public interface EmployeeMapperDynamicSQL {
+
+}
